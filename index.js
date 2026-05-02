83bbf29076d36904ecc3fd77168eff8d0156f70a
NethuNavo <nethunavo24@gmail.com>
2026-05-02 18:32:25 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const path    = require('path');
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors({
+  origin: "https://furniture-design-app-rkqx-7r1qgbsd6-nethunavos-projects.vercel.app",
+  credentials: true
+}));
+
+app.options("*", cors());
 
 // serve static files from public/
 app.use(express.static(path.join(__dirname, 'public')));
